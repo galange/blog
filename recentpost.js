33e888c2d327983b9e35ae5247b0e2cfe8a488c3
galange <fathoni.a9@gmail.com>
2017-01-04 02:48:15 +0700

    Update recentpost.js

--- recentpost.js
+++ recentpost.js
@@ -1,2 +1,57 @@
-
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('K 11(u){6.4(\'<e l="h">\');G(7 i=0;i<12;i++){7 3=u.N.3[i];7 E=3.J.$t;7 x;8(i==u.N.3.w)I;G(7 k=0;k<3.p.w;k++){8(3.p[k].10==\'Y\'){x=3.p[k].q;I}}7 9;Z{9=3.16$17.15}13(14){s=3.o.$t;a=s.v("<B");b=s.v("A=\\"",a);c=s.v("\\"",b+5);d=s.X(b+5,c-b-5);8((a!=-1)&&(b!=-1)&&(c!=-1)&&(d!="")){9=d}f 9=\'F://V.T.D/U/W/R/Q/d/1o.1n\'}6.4(\'<j l="n-J 1k">\');6.4(\'<a q="F://1l-1m.1p.D/" z="1j"><B A="\'+9+\'"/></a>\');6.4(\'<a q="\'+x+\'" z ="1b">\'+E+\'</a><1c>\');8("o"C 3){7 2=3.o.$t}f 8("y"C 3){7 2=3.y.$t}f 7 2="";7 M=/<\\S[^>]*>/g;2=2.1a(M,"");8(2.w<O){6.4(\'<m l="n-P">\');6.4(2);6.4(\'</m>\')}f{6.4(\'<m l="n-P">\');2=2.H(0,O);7 L=2.18(" ");2=2.H(0,L);6.4(2+\'...\');6.4(\'</m>\')}6.4(\'</j>\')}6.4(\'</e>\')}K 19(){r=$(\'e#h j:r\').1d().1h();$(\'e#h\').1i(r);$(\'e#h j:1g\').1e("1f")}',62,88,'||postcontent|entry|write||document|var|if|thumburl|||||ul|else||rp_plus_img||li||class|span|news|content|link|href|last|||json|indexOf|length|posturl|summary|target|src|img|in|com|posttitle|http|for|substring|break|title|function|quoteEnd|re|feed|numchars|text|hLjqmEbdtkw|AAAAAAAADMA||ggpht|_xcD4JK_dIjU|lh3|SnamIh0KTCI|substr|alternate|try|rel|rpthumbnt|numposts|catch|error|url|media|thumbnail|lastIndexOf|rpnewsticker|replace|_top|br|hide|slideDown|slow|first|remove|prepend|_blank|clearfix|tutorial|gif|noimagethumb|blogspot'.split('|'),0,{}))
+function rpthumbnt(json) {
+	document.write('<ul class="rp_plus_img">');
+	for (var i = 0; i < numposts; i++) {
+		var entry = json.feed.entry[i];
+		var posttitle = entry.title.$t;
+		var posturl;
+		if (i == json.feed.entry.length) break;
+		for (var k = 0; k < entry.link.length; k++) {
+			if (entry.link[k].rel == 'alternate') {
+				posturl = entry.link[k].href;
+				break
+			}
+		}
+		var thumburl;
+		try {
+			thumburl = entry.media$thumbnail.url
+		} catch(error) {
+			s = entry.content.$t;
+			a = s.indexOf("<img");
+			b = s.indexOf("src=\"", a);
+			c = s.indexOf("\"", b + 5);
+			d = s.substr(b + 5, c - b - 5);
+			if ((a != -1) && (b != -1) && (c != -1) && (d != "")) {
+				thumburl = d
+			} else thumburl = 'http://3.bp.blogspot.com/-L1tmPNWVc8I/WB0PLGjfPCI/AAAAAAAABzA/bCIb0kYWqlIXpXRNlxXqlHwQKJGOi1_NgCK4B/s1600/logo.png'
+		}
+		document.write('<li class="news-title clearfix">');
+		document.write('<a href="' + posturl + '" target="_blank"><img src="' + thumburl + '"/></a>');
+		document.write('<a href="' + posturl + '" target ="_top">' + posttitle + '</a><br>');
+		if ("content" in entry) {
+			var postcontent = entry.content.$t
+		} else if ("summary" in entry) {
+			var postcontent = entry.summary.$t
+		} else var postcontent = "";
+		var re = /<\S[^>]*>/g;
+		postcontent = postcontent.replace(re, "");
+		if (postcontent.length < numchars) {
+			document.write('<span class="news-text">');
+			document.write(postcontent);
+			document.write('</span>')
+		} else {
+			document.write('<span class="news-text">');
+			postcontent = postcontent.substring(0, numchars);
+			var quoteEnd = postcontent.lastIndexOf(" ");
+			postcontent = postcontent.substring(0, quoteEnd);
+			document.write(postcontent + '...');
+			document.write('</span>')
+		}
+		document.write('</li>')
+	}
+	document.write('</ul>')
+}
+function rpnewsticker() {
+	last = $('ul#rp_plus_img li:last').hide().remove();
+	$('ul#rp_plus_img').prepend(last);
+	$('ul#rp_plus_img li:first').slideDown("slow")
+}
